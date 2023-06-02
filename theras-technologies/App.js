901ed25e22eed71b56be99f3d5b8d47d832af294
gapigo <x.gabriel12@gmail.com>
2023-06-01 22:02:38 -0300

    Adicionado Header da Theras

--- theras-technologies/App.js
+++ theras-technologies/App.js
@@ -4,9 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Router from './Router';
 import Header from './Components/header/Header';
 
 export default function App() {
-  return <Router />;
+  return (
+    <SafeAreaProvider>
+      <Header />
+      <Router />
+    </SafeAreaProvider>
+  );
 }
--- theras-technologies/Components/header/Header.js
+++ theras-technologies/Components/header/Header.js
@@ -1,19 +1,50 @@
 import { Header as HeaderComponent } from '@rneui/themed';
 import { ViewComponent } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import { StyleSheet, Text, View } from 'react-native';
+// import LinearGradient from 'react-native-linear-gradient';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#410541',
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 5,
+  },
+  header_text: {
+    fontFamily: 'Piazzolla',
+    color: '#fff',
+  },
+  image_logo: {
+    width: 22,
+    height: 22,
+  },
+});
+
+const Logo = () => {
+  return (
+    <View style={styles.header}>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Piazzolla:wght@500&family=Poppins:wght@500;700&display=swap"
+        rel="stylesheet"
+      />
+      <Image
+        style={styles.image_logo}
+        source={require('../../assets/images/logo_256_256.png')}
+      />
+      <Text style={styles.header_text}>THERAS</Text>
+    </View>
+  );
+};
+
+const DummyLinearGradient = (content) => {
+  return <View style={styles.header}>{content}</View>;
+};
 
 const Header = ({ content }) => {
   return (
     <View>
-      <HeaderComponent
-        // ViewComponent={LinearGradient}
-        linearGradientProps={{
-          colors: ['red', 'pink'],
-          start: { x: 0, y: 0.5 },
-          end: { x: 1, y: 0.5 },
-        }}
-      />
+      <HeaderComponent centerComponent={Logo} backgroundColor={'#410541'} />
       {content}
     </View>
   );
